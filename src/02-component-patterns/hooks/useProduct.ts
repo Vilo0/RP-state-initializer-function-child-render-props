96bbe4870cffc42ef5cac52fbc7cf8b385ce41a7
Edgard Vilo <evilo@hostyweb.cl>
2022-02-06 20:24:43 -0300

    Clase 99: Mostrar el valor inicial en el componente

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { onChangeArgs, Product, InitialValues } from '../interfaces/interfaces';
 
 
@@ -13,6 +13,7 @@ interface useProductArgs {
 export const useProduct = ({ onChange, product, value = 0, initialValues }: useProductArgs) => {
 
     const [ counter, setCounter ] = useState<number>( initialValues?.count || value );
+    const isMounted = useRef(false);
     console.log(initialValues?.count);
 
     const increaseBy = ( value: number ) => {
@@ -22,10 +23,18 @@ export const useProduct = ({ onChange, product, value = 0, initialValues }: useP
 
         onChange && onChange({ count: newValue, product });
     }
+    
+    useEffect(() => {
+        if (!isMounted.current) return;
+        setCounter( value );
+    }, [ value ]);
 
     useEffect(() => {
-        setCounter( value );
-    }, [ value ])
+      isMounted.current = true;
+    }, []);
+
+
+    
 
     return {
         counter,
