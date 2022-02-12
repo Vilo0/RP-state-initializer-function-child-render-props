cfca3515f80edbee1b134870d67e4648247e2c15
Edgard Vilo <evilo@hostyweb.cl>
2022-02-12 14:57:51 -0300

    Clase 132: Formik - Obtener información del formulario

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,35 +1,54 @@
+import { useFormik } from 'formik';
+
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
-  return (
-    <div>
-        <h1>Formik Basic Tutorial</h1>
+    const { handleChange, values, handleSubmit } = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        onSubmit: values => {
+            console.log(values);
+        }
+    });
 
-        <form noValidate>
-            <label htmlFor="firstName">First Name</label>
-            <input 
-                type="text" 
-                name="firstName"
-            />
-            <span>First name is required</span>
+    return (
+        <div>
+            <h1>Formik Basic Tutorial</h1>
 
-            <label htmlFor="lastName">Last Name</label>
-            <input 
-                type="text" 
-                name="lastName"
-            />
-            <span>Last name is required</span>
+            <form onSubmit={ handleSubmit } noValidate>
+                <label htmlFor="firstName">First Name</label>
+                <input 
+                    type="text" 
+                    name="firstName"
+                    onChange={ handleChange }
+                    value={ values.firstName }
+                />
+                <span>First name is required</span>
 
-            <label htmlFor="email">Email Address</label>
-            <input 
-                type="email" 
-                name="email"
-            />
-            <span>Email is required</span>
-            <span>Check for an valid email format</span>
+                <label htmlFor="lastName">Last Name</label>
+                <input 
+                    type="text" 
+                    name="lastName"
+                    onChange={ handleChange }
+                    value={ values.lastName }
+                />
+                <span>Last name is required</span>
 
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-  )
+                <label htmlFor="email">Email Address</label>
+                <input 
+                    type="email" 
+                    name="email"
+                    onChange={ handleChange }
+                    value={ values.email }
+                />
+                <span>Email is required</span>
+                <span>Check for an valid email format</span>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
