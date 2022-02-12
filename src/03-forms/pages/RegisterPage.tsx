dce3a76ea74adfbee0b69120478bc7e3575e94a9
Edgard Vilo <evilo@hostyweb.cl>
2022-02-12 11:18:11 -0300

    Clase 126: Formulario tradicional

---
+++ src/03-forms/pages/RegisterPage.tsx
@@ -0,0 +1,33 @@
+import '../styles/styles.css';
+
+export const RegisterPage = () => {
+  return (
+    <div>
+        <h1>RegisterPage</h1>
+
+        <form>
+            <input 
+                type="text"
+                placeholder="Name"
+            />
+
+            <input 
+                type="email"
+                placeholder="Email"
+            />
+
+            <input 
+                type="password"
+                placeholder="Password"
+            />
+
+            <input 
+                type="password"
+                placeholder="Repeat Password"
+            />
+
+            <button type="submit">Create</button>
+        </form>
+    </div>
+  )
+}
